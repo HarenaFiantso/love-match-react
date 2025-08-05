35a4242098e341be8cabbcd155f0b4ab5723b816
Fiantso Harena <fiantsorav@gmail.com>
2025-08-05 21:18:27 +0300

    docs: add CONTRIBUTING markdown

---
+++ src/components/LoveForm.tsx
@@ -0,0 +1,47 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useState } from 'react';
+
+export function LoveForm({ className, ...props }: React.ComponentProps<'div'>) {
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(firstName);
+    console.log(secondName);
+  };
+
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl">Love Match</CardTitle>
+          <CardDescription>Lorem ipsum dolor sit.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label>Premier prenom</Label>
+                <Input id="name1" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+              </div>
+              <div className="grid gap-3">
+                <Label>Deuxième prénom</Label>
+                <Input id="name2" type="text" value={secondName} onChange={(e) => setSecondName(e.target.value)} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Tester la compatibilité
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
