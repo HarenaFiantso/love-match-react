9909a6b194a96e277ca95ec03c271d971601b60a
hiraina <hei.hiraina@gmail.com>
2025-08-07 11:36:13 +0300

    Ajout de la page

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import { SplitText } from './components';
 
 export default function App() {
   return (
-    <main className="w-full h-screen flex items-center justify-center">
-      <SplitText
-        text="Hello les K5, how are you?"
-        className="text-5xl font-semibold text-center"
-        delay={100}
-        duration={0.6}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        textAlign="center"
-      />
+    <main className='bg-yellow-100 flex flex-col w-full h-screen justify-center items-center'>
+      <div className='bg-white flex flex-col w-[40vw] rounded-lg justify-center items-center px-4 py-6 gap-8 shadow-lg'>
+        <img className='w-[50px] h-[50px]' src="/public/heart.png" alt="" />
+        <h1 className='text-3xl font-bold text-blue-500'>Love Match</h1>
+        <p>Decouvrez votre compatibilité</p>
+        <input className='border rounded-lg py-1' id='Name1' type="text" />
+        <input className='border rounded-lg py-1' id='Name2' type="text" />
+        <button className='border border-black-800 bg-blue-500 text-white font-bold rounded-[5px] px-[70px] py-[5px] hover:bg-white hover:text-blue-500 hover:border-blue-500'>Submit</button>
+      </div>
     </main>
   );
 }
